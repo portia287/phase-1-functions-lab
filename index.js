ac80ce9c3b1cfb9d99a1206c445e9222a8073b9f
Portia Antwi <portiaantwi257@gmail.com>
2022-06-12 16:11:44 -0700

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+ let location = 42;
+ if(distance >= location){
+    return distance - location;
+ }
+ else{
+    return location = distance
+ }
+}
+function distanceFromHqInFeet(distance){
+    let headQuaters = distanceFromHqInBlocks(distance);
+    return headQuaters * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    //Let feetTravelled = destination - start;
+    if (start > destination){
+        return (start - destination)* 264;
+    }
+    else{
+        return (destination - start)* 264;
+        //'cannot travel that far'
+    }
+}
+function calculateFarePrice(start, destination){
+    let feetTravelled;
+    let price;
+    if(destination > start){
+     feetTravelled = (destination- start)* 264;
+
+    }
+    else{
+        feetTravelled = (start - destination)* 264; 
+    }
+    if (feetTravelled <= 400){
+        price =0;
+    }
+    else if (feetTravelled > 400 && feetTravelled <= 2000){
+        price = (feetTravelled - 400)* 0.02
+    }
+    else if (feetTravelled > 2000 && feetTravelled <= 2500){
+        price = 25
+    }
+    else{
+        price = 'cannot travel that far'
+    }
+    return price
+}
